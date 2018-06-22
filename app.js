6a827ae1a575696d517fae89de07b4853b19e5fe
1512131 <1512131@student.hcmus.edu.vn>
2018-06-22 23:09:08 +0700

    Khởi tạo project

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+var express_handlebars_sections = require('express-handlebars-sections');
+var bodyParser = require('body-parser');
+var path = require('path');
+var wnumb = require('wnumb');
+var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
+
+var app = express();
+
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: 'views/_layouts/',
+    helpers: {
+        section: express_handlebars_sections(),
+        number_format: n => {
+            var nf = wnumb({
+                thousand: ','
+            });
+            return nf.to(n);
+        }
+    }
+}));
+
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.listen(3000, () => {
+    console.log('Site running on port 3000');
+});
